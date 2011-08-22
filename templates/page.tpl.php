a5b584aadc9ab9628f73b741e8f98fad5ade8214
Miguel Trindade <migueltrindadejr@gmail.com>
2011-08-22 01:20:49 -0300

    fluid layout and css fixes

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -67,7 +67,7 @@
  * @see template_process()
  */
  ?>
-<?php if ($page['outside_top']) { ?>
+<?php if($page['outside_top']) { ?>
 <div id="outside-top"><?php print render($page['outside_top']); ?></div><!-- #outside-top -->
 <?php } ?>
 
@@ -77,27 +77,27 @@
   
     <header id="header-page" class="clearfix">
       
-      <?php if ($page['header_top']) { ?>
+      <?php if($page['header_top']) { ?>
       <div id="header-top" class="clearfix"><?php print render($page['header_top']); ?></div><!--#header-top-->
       <?php } ?>
       
-      <?php if ($logo || $site_name || $site_slogan || $page['header']) { ?>
+      <?php if($logo || $site_name || $site_slogan || $page['header']) { ?>
       
       <div id="header-middle" class="clearfix">
         
-        <?php if ($logo || $site_name || $site_slogan) { ?>
+        <?php if($logo || $site_name || $site_slogan) { ?>
         
         <div id="logo">
         
-					<?php if ($logo) { ?>
+					<?php if($logo) { ?>
           <a id="logo" href="<?php print $front_page; ?>" title="<?php print $site_name; ?>"> <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" /> </a>
           <?php } ?>
           
-          <?php if ($site_name) { ?>
+          <?php if($site_name) { ?>
             <h1 title="<?php print $site_name; ?>" id="site-name"><a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>"><?php print $site_name; ?></a></h1>
           <?php } ?>
           
-          <?php if ($site_slogan) { ?>         
+          <?php if($site_slogan) { ?>         
           <h2 title="<?php print $site_slogan; ?>" id="site-slogan"><?php print $site_slogan; ?></h2>
           <?php } ?>
         
@@ -111,102 +111,110 @@
       
       <?php } ?>
      
-      <?php if ($page['header_bottom']) { ?>
+      <?php if($page['header_bottom']) { ?>
       <div id="header-bottom" class="clearfix"><?php print render($page['header_bottom']); ?></div><!--#header-bottom-->
       <?php } ?>
       
     </header><!-- #header-page -->
     
-    <?php if ($page['preface_top']) { ?>
+    <?php if($page['preface_top']) { ?>
     <div id="preface-top" class="clearfix"><?php print render($page['preface_top']); ?></div><!-- #preface-top -->
     <?php } ?>
     
-    <?php if ($page['sidebar_first']) { ?>
-    <div id="sidebar-first" class="sidebar"><?php print render($page['sidebar_first']); ?> </div><!-- #sidebar-first -->
-    <?php } ?>
-    
-    <div id="main">
-    
-      <?php if ($page['preface_bottom']) { ?>
-      <div id="preface-bottom" class="clearfix"><?php print render($page['preface_bottom']); ?></div><!-- #preface-bottom -->
-      <?php } ?>
+    <div class="wrapper-columns-outside">
+      
+      <div id="main">
 
-      <div id="wrapper-content">
-        
-        <?php if ($page['content_top']) { ?>
-        <div id="content-top" class="clearfix"> <?php print render($page['content_top']); ?> </div><!-- #content-top -->
+        <?php if($page['preface_bottom']) { ?>
+        <div id="preface-bottom" class="clearfix"><?php print render($page['preface_bottom']); ?></div><!-- #preface-bottom -->
         <?php } ?>
         
-        <div id="content-middle" class="clearfix">
+        <div class="wrapper-columns-inside">
           
-          <?php if ($breadcrumb) { ?>
-          <nav id="breadcrumb"><?php print $breadcrumb; ?> </nav><!-- #breadcrumb -->
-          <?php } ?>
-          
-          <?php if ($page['highlighted']){ ?>
-          <div id="highlighted"><?php print render($page['highlighted']); ?></div><!-- #highlighted -->        
-          <?php } ?>
-  
-          <?php print render($title_prefix); ?>
-          <?php if ($title) { print '<h2 class="page-title"><span>'. $title .'</span></h2>'; } ?>
-          <?php print render($title_suffix); ?>
-          
-          <?php if ($tabs) { ?>
-          <div id="tabs"><?php print render($tabs); ?></div>
-          <?php } ?>
+          <div id="wrapper-content">
 
-          <?php if ($messages) { ?>
-          <div id="messages" class="clear"><?php print $messages; ?></div><!-- #messages -->
-          <?php } ?>
+            <?php if($page['content_top']) { ?>
+            <div id="content-top" class="clearfix"> <?php print render($page['content_top']); ?> </div><!-- #content-top -->
+            <?php } ?>
 
-          <?php if ($page['help']) { ?>
-          <div id="help"><?php print render($page['help']); ?></div><!-- #help -->
+            <div id="content-middle" class="clearfix">
+
+              <?php if($breadcrumb) { ?>
+              <nav id="breadcrumb"><?php print $breadcrumb; ?> </nav><!-- #breadcrumb -->
+              <?php } ?>
+
+              <?php if($page['highlighted']){ ?>
+              <div id="highlighted"><?php print render($page['highlighted']); ?></div><!-- #highlighted -->        
+              <?php } ?>
+
+              <?php print render($title_prefix); ?>
+              <?php if($title) { print '<h2 class="page-title"><span>'. $title .'</span></h2>'; } ?>
+              <?php print render($title_suffix); ?>
+
+              <?php if($tabs) { ?>
+              <div id="tabs"><?php print render($tabs); ?></div>
+              <?php } ?>
+
+              <?php if($messages) { ?>
+              <div id="messages" class="clear"><?php print $messages; ?></div><!-- #messages -->
+              <?php } ?>
+
+              <?php if($page['help']) { ?>
+              <div id="help"><?php print render($page['help']); ?></div><!-- #help -->
+              <?php } ?>
+
+              <?php if($action_links){ ?>
+              <div id="action-links">
+                <ul><?php print render($action_links); ?></ul>
+              </div><!-- #action-links -->
+              <?php } ?>
+
+
+              <?php print render($page['content']); ?> 
+
+              <span class="clear"></span> 
+
+            </div><!-- #content-middle -->
+
+            <?php if($page['content_bottom']) { ?>
+            <div id="content-bottom" class="clearfix"><?php print render($page['content_bottom']); ?></div><!-- #content-bottom -->
+            <?php } ?>
+
+          </div><!-- #wrapper-content -->
+
+          <?php if($page['sidebar_second']){ ?>
+          <aside id="sidebar-second" class="sidebar"> <?php print render($page['sidebar_second']) ?> </aside><!-- #sidebar-second -->
           <?php } ?>
-          
-          <?php if ($action_links){ ?>
-          <div id="action-links">
-            <ul><?php print render($action_links); ?></ul>
-          </div><!-- #action-links -->
-          <?php } ?>
-  
-          
-          <?php print render($page['content']); ?> 
-          
-          <span class="clear"></span> 
         
-        </div><!-- #content-middle -->
+        </div>
         
-        <?php if ($page['content_bottom']) { ?>
-        <div id="content-bottom" class="clearfix"><?php print render($page['content_bottom']); ?></div><!-- #content-bottom -->
+        <?php if($page['postscript_top']) { ?>
+        <div id="postscript-top" class="clearfix"><?php print render($page['postscript_top']); ?></div><!-- #postscript-top -->
         <?php } ?>
+
+      </div><!-- #main -->
       
-      </div><!-- #wrapper-content -->
-      
-      <?php if ($page['sidebar_second']){ ?>
-      <aside id="sidebar-second" class="sidebar"> <?php print render($page['sidebar_second']) ?> </aside><!-- #sidebar-second -->
+      <?php if($page['sidebar_first']) { ?>
+      <aside id="sidebar-first" class="sidebar"><?php print render($page['sidebar_first']); ?> </aside><!-- #sidebar-first -->
       <?php } ?>
       
-      <?php if ($page['postscript_top']) { ?>
-      <div id="postscript-top" class="clearfix"><?php print render($page['postscript_top']); ?></div><!-- #postscript-top -->
-      <?php } ?>
-      
-    </div><!-- #main -->
+    </div>
     
-    <?php if ($page['postscript_bottom']) { ?>
+    <?php if($page['postscript_bottom']) { ?>
     <div id="postscript-bottom" class="clearfix"><?php print render($page['postscript_bottom']); ?></div><!-- #postscript-bottom -->
     <?php } ?>
     
     <footer id="footer-page" class="clearfix">
       
-      <?php if ($page['footer_top']) { ?>
+      <?php if($page['footer_top']) { ?>
       <div id="footer-top" class="clearfix"><?php print render($page['footer_top']); ?></div> <!-- #footer-top -->
       <?php } ?>
       
-      <?php if ($page['footer']) { ?>
+      <?php if($page['footer']) { ?>
       <div id="footer-middle" class="clearfix"><?php print render($page['footer']); ?></div><!-- #footer-middle -->
       <?php } ?>
       
-      <?php if ($feed_icons || $page['footer_bottom']) { ?>
+      <?php if($feed_icons || $page['footer_bottom']) { ?>
       <div id="footer-bottom" class="clear"> <?php print $feed_icons ?> <?php print render($page['footer_bottom']); ?> </div><!-- #footer-bottom -->
       <?php } ?>
        
@@ -216,7 +224,7 @@
 
 </div><!-- #wrapper-container -->
 
-<?php if ($page['outside_bottom']) { ?>
+<?php if($page['outside_bottom']) { ?>
 <div id="outside-bottom" class="clear"><?php print render($page['outside_bottom']); ?></div>
 <!-- #outside-bottom -->
 <?php } ?>
